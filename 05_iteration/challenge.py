5a8dfd4c17ad0c2989cbd9c1aa91711d596ab4cf
Theo <theo.piron.pro@gmail.com>
2026-05-21 12:02:57 +0200

    challenge

--- 05_iteration/challenge.py
+++ 05_iteration/challenge.py
@@ -1,14 +1,19 @@
 # Pavage 2 couleur -> A(n,p) = n^p (n = 2 motifs, p = tirages|nombre de pavés)
 # Nombre de motifs pour k pavés -> Fibonacci(k)
-def pavage(longueur: int, motifs: int = 2, largeur: int = 2, motif_size: tuple[int, int] = (1, 2)) -> int:
+def pavage(longueur: int, couleurs: int = 2) -> int:
+    if longueur == 0 :
+        return 0
     if longueur == 1 :
-        return motifs
+        return couleurs
+    if longueur == 2:
+        return longueur * couleurs**2
     
-    return motifs**longueur* motifs**(longueur-1)
+    return 2 * pavage(longueur - 1) + 4 * pavage(longueur - 2)
 
 
 def main() -> None:
-    print(pavage(4))
+    for i in range(10):
+        print(pavage(i))
     return
 
 if __name__ == "__main__":
